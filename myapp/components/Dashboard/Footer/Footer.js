50916a0c4cfd3a05197e4d858965dfc7aceb0a44
thejayadad <74875595+thejayadad@users.noreply.github.com>
2024-02-07 08:17:36 -0500

    update

--- myapp/components/Dashboard/Footer/Footer.js
+++ myapp/components/Dashboard/Footer/Footer.js
@@ -1,11 +1,31 @@
-import React from 'react'
+'use client'
+import React from 'react';
+import { FiFacebook, FiTwitter, FiInstagram, FiGithub } from 'react-icons/fi';
 
 const Footer = () => {
   return (
-    <footer className='px-4 py-8'>
-        Footer
-    </footer>
-  )
-}
+    <div className="max-w-screen-xl px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+      <a href="#" className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+        <span className="ml-3 text-xl">The Memory Bank</span>
+      </a>
+      <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">©2024 Thejayadad
+      </p>
+      <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+        <a href="#" className="text-gray-500 mx-2">
+          <FiFacebook size={20} />
+        </a>
+        <a href="#" className="text-gray-500 mx-2">
+          <FiTwitter size={20} />
+        </a>
+        <a href="#" className="text-gray-500 mx-2">
+          <FiInstagram size={20} />
+        </a>
+        <a href="#" className="text-gray-500 mx-2">
+          <FiGithub size={20} />
+        </a>
+      </span>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
